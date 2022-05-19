ecb9c45b41540a35dcd344447de0ff9ee7e96ba2
Igor Volkov <igor.volkov94@icloud.com>
2022-05-19 08:07:08 +0300

    add robots, helper_functions and index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="RU">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Тестовое задание</title>
+</head>
+<body>
+    <a href="task1.php">Задача 1</a>
+    <a href="task2.php">Задача 2</a>
+    <a href="task3.php">Задача 3</a>
+    <a href="task4.php">Задача 4</a>
+</body>
+</html>
